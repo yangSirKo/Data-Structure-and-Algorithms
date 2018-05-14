4f6e8467349b199c02a9c9131a8c4a71ab107d7c
yangjinpeng <1415946318@qq.com>
2018-05-15 00:13:16 +0800

    第三个版本的并查集，基于size的优化

--- src/com/ccyang/_06_UnionFind/version3/Main.java
+++ src/com/ccyang/_06_UnionFind/version3/Main.java
@@ -1,5 +1,4 @@
-package com.ccyang._06_UnionFind.version2;
-
+package com.ccyang._06_UnionFind.version3;
 
 /**
  * 测试 UF1 & UF2
@@ -9,16 +8,20 @@ public class Main {
     public static void main(String[] args) {
 
         // 测试数据规模
-        int n = 10000;
+        int n = 1000000;
 
         // 虽然 isConnected 时间复杂度为 O(1), 但是 union 时间复杂度为 O(n)
-        // 所以整体时间复杂度为 O(n^2).  // n 次 union操作
-        UnionFindTestHelper.testUF1(n);
+        // 所以整体时间复杂度为 O(n^2).
+//        UnionFindTestHelper.testUF1(n);   // 100000 级数据该方法太慢太慢
 
         // 对于UF2来说, 其时间性能是O(n*h)的, h为并查集表达的树的最大高度
         // 这里严格来讲, h和log(n)没有关系, 不过大家可以简单这么理解
         // 后续会对h进行优化, 总体而言, 这个h是远小于n的
         // 所以我们实现的UF2测试结果远远好于UF1, n越大越明显:)
         UnionFindTestHelper.testUF2(n);
+
+        // 对于UF3来说, 其时间性能依然是O(n*h)的, h为并查集表达的树的最大高度
+        // 但由于UF3能更高概率的保证树的平衡, 所以性能更优
+        UnionFindTestHelper.testUF3(n);
     }
 }
--- src/com/ccyang/_06_UnionFind/version3/UnionFind3.java
+++ src/com/ccyang/_06_UnionFind/version3/UnionFind3.java
@@ -1,4 +1,62 @@
 package com.ccyang._06_UnionFind.version3;
 
+/**
+ * 第三版的 UnionFind
+ *
+ * 基于 size的优化
+ */
 public class UnionFind3 {
+
+    private int[] parent;
+    private int[] sz;
+    private int count;
+
+    public UnionFind3(int count) {
+        this.count = count;
+        parent = new int[count];
+        sz = new int[count];
+
+        // 初始化, 每一个parent[i]指向自己, 表示每一个元素自己自成一个集合
+        for(int i=0; i < count; i++){
+            parent[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    // 查找过程, 查找元素p所对应的集合编号
+    // O(h)复杂度, h为树的高度
+    public int find(int p){
+        assert count > p && p >= 0;
+
+        while(p != parent[p])
+            p = parent[p];
+        return p;
+    }
+
+    // 查看元素p和元素q是否所属一个集合
+    // O(h)复杂度, h为树的高度
+    public boolean isConnected(int p, int q){
+        return find(p) == find(q);
+    }
+
+    // 合并元素p和元素q所属的集合
+    // O(h)复杂度, h为树的高度
+    public void unionElements(int p, int q){
+
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot)
+           return ;
+
+        // 根据两个元素所在树的元素个数不同判断合并方向
+        // 将元素个数少的集合合并到元素个数多的集合上
+        if(sz[pRoot] < sz[qRoot]){
+            parent[pRoot] = qRoot;
+            sz[qRoot] += sz[pRoot];
+        }else{
+            parent[qRoot] = pRoot;
+            sz[pRoot] += sz[qRoot];
+        }
+    }
 }
