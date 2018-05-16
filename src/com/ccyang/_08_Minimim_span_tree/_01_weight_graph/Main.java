d890df88dfb454ade752a593663cb635ac4143ab
yangjinpeng <1415946318@qq.com>
2018-05-16 23:58:11 +0800

    实现 带权图，基于邻接表和邻接矩阵

--- src/com/ccyang/_08_Minimim_span_tree/_01_weight_graph/Main.java
+++ src/com/ccyang/_08_Minimim_span_tree/_01_weight_graph/Main.java
@@ -1,4 +1,25 @@
 package com.ccyang._08_Minimim_span_tree._01_weight_graph;
 
 public class Main {
+
+    // 测试通过文件读取图的信息
+    public static void main(String[] args) {
+
+        // 使用两种图的存储方式读取testG1.txt文件
+        String filename = ".\\src\\com\\ccyang\\_08_Minimim_span_tree\\testG1";
+
+        SparseWeightedGraph<Double> g1 = new SparseWeightedGraph<Double>(8, false);
+        ReadWeightedGraph readGraph1 = new ReadWeightedGraph(g1, filename);
+        System.out.println("test G1 in Sparse Weighted Graph:");
+        g1.show();
+
+        System.out.println();
+
+        DenseWeightedGraph<Double> g2 = new DenseWeightedGraph<Double>(8, false);
+        ReadWeightedGraph readGraph2 = new ReadWeightedGraph(g2 , filename );
+        System.out.println("test G1 in Dense Graph:");
+        g2.show();
+
+        System.out.println();
+    }
 }
